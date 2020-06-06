3380aa33036d7c4369be7fd18c0f90f16f1abdd3
JeanCarlosMongeMendez <42584455+JeanCarlosMongeMendez@users.noreply.github.com>
2020-06-05 20:27:52 -0600

    Initial Project
    Empty

---
+++ GuerrillaAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GuerrillaAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
