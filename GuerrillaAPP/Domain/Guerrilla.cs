4804919f30d87b4edd60dc83810988e5500f6e8d
Daniel fonseca Solano <31803434+dfs11@users.noreply.github.com>
2020-06-11 22:58:20 -0600

    Domain sin relaciones

---
+++ GuerrillaAPP/Domain/Guerrilla.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuerrillaAPP.Domain
+{
+    public class Guerrilla
+    {
+        public Guerrilla(int idGuerrilla,String nombreGuerrilla,String correoGuerrilla) {
+            this.idGuerrilla = idGuerrilla;
+            this.nombreGuerrilla = nombreGuerrilla;
+            this.correoGuerrilla = correoGuerrilla;
+        }
+        public int idGuerrilla { get; set; }
+        public String nombreGuerrilla { get; set; }
+        public String correoGuerrilla { get; set; }
+    }
+}
