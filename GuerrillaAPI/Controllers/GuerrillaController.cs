28d00c89ba6458a1c4a3f1dc979ac950f95e0af9
JeanCarlosMongeMendez <42584455+JeanCarlosMongeMendez@users.noreply.github.com>
2020-06-20 18:26:04 -0600

    units added

--- GuerrillaAPI/Controllers/GuerrillaController.cs
+++ GuerrillaAPI/Controllers/GuerrillaController.cs
@@ -50,8 +50,8 @@ namespace GuerrillaAPI.Controllers
                     guerrillas = _context.Guerrilla.Where(g => g.Faction.Equals(faction)).ToList();
 
                 List<guerrillas> guerrillasDTO = new List<guerrillas>();
-                
-                foreach(Guerrilla guerrilla in guerrillas)
+
+                foreach (Guerrilla guerrilla in guerrillas)
                 {
                     guerrillasDTO.Add(new guerrillas { guerrillaName = guerrilla.Name, faction = guerrilla.Faction, rank = guerrilla.Rank });
                 }
@@ -113,24 +113,103 @@ namespace GuerrillaAPI.Controllers
             }
         }
 
+        [HttpPut("{name}/units")]
+        public string BuyUnits(string name, units units)
+        {
+            if (units.buildings == null)
+                units.buildings = new buildings();
+            if (units.army == null)
+                units.army = new army();
+
+            List<Unit> allUnits = _context.Unit.ToList();
+            List<GuerrillaResources> guerrillaResources = _context.GuerrillaResources.Where(g => g.Guerrilla.Equals(name)).ToList();
+
+            #region calculate costs
+            int oilCost = units.army.assault * allUnits.Where(u => u.Name.Equals(_assault)).Single().Oil +
+                    units.army.engineer * allUnits.Where(u => u.Name.Equals(_engineer)).Single().Oil +
+                    units.army.tank * allUnits.Where(u => u.Name.Equals(_tank)).Single().Oil + 
+                    units.buildings.bunker * allUnits.Where(u=>u.Name.Equals(_bunker)).Single().Oil;
+
+            int moneyCost = units.army.assault * allUnits.Where(u => u.Name.Equals(_assault)).Single().Money +
+                units.army.engineer * allUnits.Where(u => u.Name.Equals(_engineer)).Single().Money +
+                units.army.tank * allUnits.Where(u => u.Name.Equals(_tank)).Single().Money +
+                units.buildings.bunker * allUnits.Where(u => u.Name.Equals(_bunker)).Single().Money;
+
+            int peopleCost = units.army.assault * allUnits.Where(u => u.Name.Equals(_assault)).Single().People +
+                units.army.engineer * allUnits.Where(u => u.Name.Equals(_engineer)).Single().People +
+                units.army.tank * allUnits.Where(u => u.Name.Equals(_tank)).Single().People +
+                units.buildings.bunker * allUnits.Where(u => u.Name.Equals(_bunker)).Single().People;
+            #endregion
+
+            GuerrillaResources guerrillaOil = guerrillaResources.Where(r => r.Resource.Equals(_oil)).Single();
+            GuerrillaResources guerrillaMoney = guerrillaResources.Where(r => r.Resource.Equals(_money)).Single();
+            GuerrillaResources guerrillaPeople = guerrillaResources.Where(r => r.Resource.Equals(_people)).Single();
+            if ((guerrillaOil.Quantity - oilCost) >= 0 && (guerrillaMoney.Quantity-moneyCost)>=0 && (guerrillaPeople.Quantity - peopleCost)>0)
+            {
+                if (units.army.assault > 0)
+                {
+                    GuerrillaUnits assaultNewUnits = new GuerrillaUnits();
+                    assaultNewUnits.Guerrilla = name;
+                    assaultNewUnits.Unit = _assault;
+                    assaultNewUnits.Quantity = units.army.assault;
+                    _context.GuerrillaUnits.Update(assaultNewUnits);
+                }
+
+                if(units.army.engineer > 0)
+                {
+                    GuerrillaUnits engineerNewUnits = new GuerrillaUnits();
+                    engineerNewUnits.Guerrilla = name;
+                    engineerNewUnits.Unit = _engineer;
+                    engineerNewUnits.Quantity = units.army.engineer;
+                    _context.GuerrillaUnits.Update(engineerNewUnits);
+                }
+                
+                if(units.army.tank > 0)
+                {
+                    GuerrillaUnits tankNewUnits = new GuerrillaUnits();
+                    tankNewUnits.Guerrilla = name;
+                    tankNewUnits.Unit = _tank;
+                    tankNewUnits.Quantity = units.army.tank;
+                    _context.GuerrillaUnits.Update(tankNewUnits);
+                }
+
+                if (units.buildings.bunker > 0)
+                {
+                    GuerrillaUnits bunkerNewUnits = new GuerrillaUnits();
+                    bunkerNewUnits.Guerrilla = name;
+                    bunkerNewUnits.Unit = _bunker;
+                    bunkerNewUnits.Quantity = units.buildings.bunker;
+                    _context.GuerrillaUnits.Add(bunkerNewUnits);
+                }
+
+                guerrillaOil.Quantity = guerrillaOil.Quantity - oilCost;
+                guerrillaMoney.Quantity = guerrillaMoney.Quantity - moneyCost;
+                guerrillaPeople.Quantity = guerrillaPeople.Quantity - peopleCost;
+                _context.GuerrillaResources.Update(guerrillaOil);
+                _context.SaveChanges();
+            }
+            return GetGuerrillaDTO(name);
+        }
+
         private string GetGuerrillaDTO(string name)
         {
             //load guerrilla
-            var guerrilla = (from guerrillaDB in _context.Guerrilla where guerrillaDB.Name.Equals(name) 
+            var guerrilla = (from guerrillaDB in _context.Guerrilla
+                             where guerrillaDB.Name.Equals(name)
                              select new guerrilla()
-                               {
-                                   guerrillaName = guerrillaDB.Name,
-                                   name = guerrillaDB.Name,
-                                   email = guerrillaDB.Email,
-                                   faction = guerrillaDB.Faction,
-                                   rank = guerrillaDB.Rank,
-                                   timestamp = guerrillaDB.Timestamp
-                               }).Single();
+                             {
+                                 guerrillaName = guerrillaDB.Name,
+                                 name = guerrillaDB.Name,
+                                 email = guerrillaDB.Email,
+                                 faction = guerrillaDB.Faction,
+                                 rank = guerrillaDB.Rank,
+                                 timestamp = guerrillaDB.Timestamp
+                             }).Single();
 
             //load resources
             resources resources = new resources();
             List<GuerrillaResources> resourcesDB = _context.GuerrillaResources.Where(r => r.Guerrilla.Equals(name)).ToList();
-            foreach(GuerrillaResources guerrillaResources in resourcesDB)
+            foreach (GuerrillaResources guerrillaResources in resourcesDB)
             {
                 if (guerrillaResources.Resource.Equals(_oil))
                     resources.oil = guerrillaResources.Quantity;
---
+++ GuerrillaAPI/Model/DTO/units.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuerrillaAPI.Model.DTO
+{
+    public class units
+    {
+        public army army { get; set; }
+        public buildings buildings { get; set; }
+    }
+}
